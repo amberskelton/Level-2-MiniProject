31ff2e22d9f9d4de9ccc168c6f201a5355b6fdc9
amberskelton <168379622+amberskelton@users.noreply.github.com>
2026-04-17 23:13:43 +1200

    added choice between cat and dog

---
+++ v1Main.py
@@ -0,0 +1,27 @@
+# the following program is a simple multichoice quiz to help you decide which breed of cat/dog to get
+
+import Functions1
+import sys
+print('Instructions')
+
+while True:
+    print('---------------------------------------')
+    print('Do you want a cat or a dog?')
+    print('\nOptions:')
+    print('1. Cat\n2. Dog\n3.No')
+    answer=input('Enter your answer: ')
+    if answer=='Cat'.lower():
+        print('---------------------------------------')
+        Functions1.catPicked()
+        break
+    elif answer=='Dog'.lower():
+        print('---------------------------------------')
+        Functions1.dogPicked()
+        break
+    elif answer=='No'.lower():
+        print('---------------------------------------')
+        print('ok bye')
+        sys.exit()
+    else:
+        print('---------------------------------------')
+        print('Please enter "dog" or "cat".')
